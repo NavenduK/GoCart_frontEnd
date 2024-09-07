058adc36771709f4cdb2c8fbcaf8f539b0c5e06a
Navandu Kantha <navendukantha2002@gmail.com>
2024-09-07 19:00:54 +0530

    build issue fix

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,4 +1,4 @@
-export const SERVER_DOMAIN: string = 'https://go-cart-backend.vercel.app/'
+export const SERVER_DOMAIN: string = 'https://gocart-backend.onrender.com/'
 
 export const isValidEmailRegex =
   /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
